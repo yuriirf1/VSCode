45700f719a4fa1af7f8bafb27877fd23a453d1e7
Yury Shalyapin <monchegorsk1@mail.ru>
2024-09-15 21:18:32 +0300

    Second commit: int a

---
+++ Prog.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Enter your name: ";
+    std::string name;
+    std::cin >> name;
+    std::cout << "Hello, " << name << "!" << std::endl;
+    return 0;
+} 
