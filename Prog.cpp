416cf47f04c7b0ddc51da7828ff6f6cff76e54e0
Yury Shalyapin <monchegorsk1@mail.ru>
2024-09-20 02:05:37 +0300

    begin2

--- Prog.cpp
+++ Prog.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
 #include <cmath>
-double perimeterOFSquare(double );//begin1 prototypr of function Begin1
+double perimeterOfSquare(double );//begin1 prototype of functionr of function Begin1
+double squareArea(double );//begin2
 
 int main()
 {
     double a;
     std::cout << "Enter side of square: ";
     std::cin >> a;
-    double result = perimeterOFSquare(a);
-    std::cout << result << std::endl; 
+    double result = perimeterOfSquare(a);
+    std::cout << "Perimeter of square: " << result << std::endl; 
+    result = squareArea(a);
+    std::cout << "Area of square: " << result << std::endl;
     
     return 0;
-} 
-
-double perimeterOFSquare(double side)//begin1
+}
+double perimeterOfSquare(double side)//begin1
 {
-    return side*4;
+    return side * 4;
+}
+double squareArea(double side)//begin2
+{
+    return std::pow(side, 2);
 }
