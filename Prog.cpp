e5ad3252861251b6ee74263bd499d6ba159dae7f
Yury Shalyapin <monchegorsk1@mail.ru>
2024-09-19 23:18:30 +0300

    Error with russian language

--- Prog.cpp
+++ Prog.cpp
@@ -1,13 +1,10 @@
 #include <iostream>
 
-int main() {
-    int a = 7;/////////////////////////////
-    std::cout << "Hello, World!" << std::endl;
-    double b = std::pow(a, 1.0/3.0);/////////////////////////
-    std::cout << "Enter your name: ";
-    std::string name;
-    std::cin >> name;
-    int c;///////////
-    std::cout << "Hello, " << name << "!" << std::endl;
+int main()
+{
+    system("chcp 1251");
+    std::cout << "Юра:)\n";
+
+    system("pause");
     return 0;
 } 
