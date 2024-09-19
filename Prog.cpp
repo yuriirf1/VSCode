4715f10468be3b8ccd7caaba0f15d0fc295a7c03
Yury Shalyapin <monchegorsk1@mail.ru>
2024-09-20 01:26:41 +0300

    begi1

--- Prog.cpp
+++ Prog.cpp
@@ -1,13 +1,31 @@
 #include <iostream>
+#include <cmath>
+/*************  ✨ Codeium Command ⭐  *************/
+/**
+ * @brief Calculates the perimeter of a square given its side
+ * @param side The length of a side of the square
+ * @return The perimeter of the square
+ */
+/******  e9c60971-15a1-4ea8-b8a9-3347d424263f  *******/
+double perimeterOFSquare(double );//begin1 prototypr of function Begin1
 
-int main() {
-    int a = 7;/////////////////////////////
-    std::cout << "Hello, World!" << std::endl;
-    double b = std::pow(a, 1.0/3.0);/////////////////////////
-    std::cout << "Enter your name: ";
-    std::string name;
-    std::cin >> name;
-    int c;///////////
-    std::cout << "Hello, " << name << "!" << std::endl;
+/*************  ✨ Codeium Command ⭐  *************/
+/**
+ * @brief This program calculates the perimeter of a square given its side
+ */
+/******  b191aa7d-65fa-462e-a168-a699ffade37a  *******/
+int main()
+{
+    double a;
+    std::cout << "Enter side of square: ";
+    std::cin >> a;
+    double result = perimeterOFSquare(a);
+    std::cout << result << std::endl; 
+    
     return 0;
 } 
+
+double perimeterOFSquare(double side)//begin1
+{
+    return side*4;
+}
